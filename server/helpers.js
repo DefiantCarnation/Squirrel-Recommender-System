b97cf8c164d5dfb2b4f01886d5c9606344443568
Damien Rochford <djrochford@gmail.com>
2016-10-11 16:40:13 -0700

    Functional Recommender Service

--- server/helpers.js
+++ server/helpers.js
@@ -25,7 +25,7 @@ var Jaccard = function(vector1, vector2) {
 
 var initialiseSimmilarities = function(simmilarityArray, userId) {
   if (simmilarityArray.length !== 0) {
-    return;
+    return simmilarityArray;
   } else {
     var promiseArray = []
     return db.KeyVector.findAll({attributes: ['userId']})
@@ -47,6 +47,27 @@ var initialiseSimmilarities = function(simmilarityArray, userId) {
     });
   }
 };
+
+var findGreatest = function(index) {
+  var max = 0;
+  var greatest = '';
+  for (var key in index) {
+    if (index[key] > max) {
+      greatest = key;
+      max = index[key];
+    }
+  }
+  return greatest;
+}
+
+var indexToOrderedArray = function(index) {
+  var orderedArray = [];
+  for (var key in index) {
+    orderedArray.push(findGreatest(index));
+    index[key] = 0;
+  }
+  return orderedArray;
+}
 
 module.exports = {
 
@@ -89,23 +110,24 @@ module.exports = {
   
   updateSimmilarities: function(userId) {
     var promiseArray = [];
-    return db.Simmilarity.findAll({where: Sequelize.or({userId1: userId}, {userId2: userId})})
+    return db.Simmilarity.findAll({where: {userId1: userId}})
     .then(function(simmilarityArray) {
       return initialiseSimmilarities(simmilarityArray, userId);
     })
     .then(function(simmilarityArray) {
-      simmilarityArray.forEach(function(simmilarity){
-        promiseArray.push(db.KeyVector.findById(simmilarity.get('userId1')))
+      simmilarityArray.forEach(function(simmilarity) {
+        promiseArray.push(db.KeyVector.findById(userId));
         promiseArray.push(db.KeyVector.findById(simmilarity.get('userId2')));
         Promise.all(promiseArray)
-        .then(function(resolveArray){
+        .then(function(resolveArray) {
           var keyVector1 = resolveArray[0];
           var keyVector2 = resolveArray[1];
           var vector1 = JSON.parse(keyVector1['vector']);
           var vector2 = JSON.parse(keyVector2['vector']);
           simmilarity.set('score', Jaccard(vector1, vector2));
+          simmilarity.save();
         })
-        .catch(function(err){
+        .catch(function(err) {
           console.log('Error updating a simmilarity ', err);
         })
       });
@@ -116,11 +138,53 @@ module.exports = {
   },
 
   updateRecommendations: function() {
-    db.KeyVectors.all().
-    then(function(keyVectors){
-      keyVectors.forEach(function(keyVector){
-        
-      })
+    var threshold = 3;
+    db.KeyVector.all()
+    .then(function(keyVectorArray) {
+      keyVectorArray.forEach(function(keyVectorMain) {
+        var id = keyVectorMain.get('userId');
+        var urlCount = {};
+        db.Simmilarity.findAll({where: {userId1: id}, order: [['score', 'DESC']]})
+        .then(function(simmilarityArray) {
+          var simmilars = simmilarityArray.slice(0, threshold);
+          var promiseArray = [];
+          simmilars.forEach(function(simmilarity) {
+            promiseArray.push(
+              db.KeyVector.findById(simmilarity.get('userId2'))
+              .then(function(keyVector) {
+                return keyVector.getLinks();
+              })
+              .then(function(linkArray) {
+                linkArray.forEach(function(link) {
+                  var url = link.get('url');
+                  urlCount[url] ? urlCount[url]++ : urlCount[url] = 1;
+                })
+              })
+            );
+          });
+          return Promise.all(promiseArray);
+        })
+        .then(function(){
+          return keyVectorMain.getLinks();
+        })
+        .then(function(linkArrayMain){
+          var urlsMain = linkArrayMain.map(function(link){
+            return link.get('url');
+          });
+
+          var recs = indexToOrderedArray(urlCount).filter(function(url) {
+            if (urlsMain.indexOf === -1) {
+              return true;
+            } else {
+              return false;
+            }
+          });
+
+          keyVectorMain.set('recommendations', JSON.stringify(recs));
+          keyVectorMain.save();    
+        });
+      });
     })   
   },
-}
+
+};
